99dd1bd22c0be68f6bd638d10ae3673f45616faa
Parthav46 <parthav46@gmail.com>
2018-02-04 03:56:14 +0530

    Revert "Revert "Revert "Revert "controller pages added""""
    This reverts commit a833b46d8cf022a4c8dd560ddc3d49d65cae0f13.

---
+++ android/Universal_Remote/app/src/main/java/iouniversal_remote/github/parthav46/universalremote/Controller.java
@@ -0,0 +1,39 @@
+package iouniversal_remote.github.parthav46.universalremote;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.NavUtils;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class Controller extends AppCompatActivity {
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_controller);
+        setTitle(MainActivity.selected);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        ControllerViewAdapter adapter = new ControllerViewAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+}
