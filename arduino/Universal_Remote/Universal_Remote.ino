09e2c50b21140c0b52c1edb8a33c2158fc43aa63
Rushi Bhatt <rybhatt27@gmail.com>
2018-02-08 18:47:32 +0530

    arduino update

--- arduino/Universal_Remote/Universal_Remote.ino
+++ arduino/Universal_Remote/Universal_Remote.ino
@@ -1,13 +1,52 @@
+//TODO pin d8 as data selector
+// when data send low
+// freq send then high
+//TODO pin d6 ATMega reset pin high always
+//TODO serial print freq received from query ,and serial print freq d8 high when signal and low afterwards
+//TODO same for data , by default 0
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
-const char *ssid = "NETGEAR300";
-const char *password = "home29391";
+const char *ssid = "rushi";
+const char *password = "1122334455";
+
+int reset_pin = D6;
+int select  =  D8;
 
 ESP8266WebServer server ( 80 );
+
+void getfrequency(){
+      String data="";
+      for ( int i = 0; i < server.args(); i++ ) {
+       if(server.argName ( i ) == "data")
+       {
+        data=server.arg(i);
+       }
+      }
+      data = data.substring(1,data.length()-1);
+      Serial.print("frequency is");
+      Serial.println(data);
+      digitalWrite(select,HIGH);
+      server.send ( 200, "text/plain", "OK" );
+}
+
+void getdata(){
+      String data="";
+      for ( int i = 0; i < server.args(); i++ ) {
+       if(server.argName ( i ) == "data")
+       {
+        data=server.arg(i);
+       }
+      }
+      data = data.substring(1,data.length()-1);
+      Serial.print("data is");
+      Serial.println(data);
+      digitalWrite(select,LOW);
+      server.send ( 200, "text/plain", "OK" );
+}
 
 void getid(){
   String idname="node mcu";
@@ -78,10 +117,14 @@ void handleNotFound() {
 }
 
 void setup() {
-  Serial.begin ( 115200 );
+  Serial.begin ( 9600 );
   WiFi.mode ( WIFI_STA );
   WiFi.begin ( ssid, password );
-
+  
+  pinMode(reset_pin,OUTPUT);
+  pinMode(select,OUTPUT);
+  digitalWrite(reset_pin,HIGH);
+  
   // Wait for connection
   while ( WiFi.status() != WL_CONNECTED ) {
     delay ( 500 );
@@ -100,6 +143,8 @@ void setup() {
   server.on("/id",getid);
   server.on("/device",getdevice);
   server.on("/control",control);
+  server.on("/frequency",getfrequency);
+  server.on("/data",getdata);
   
   server.onNotFound ( handleNotFound );
   server.begin();
